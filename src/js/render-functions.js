0d985d7e32f738e5f1828b0e2fc012860732fa92
Krinzh <23oleg03@gmail.com>
2025-06-15 02:36:40 +0300

    refactor: rename functions and variables for better code uniqueness

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,27 +1,27 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox'
+import 'simplelightbox/dist/simple-lightbox.min.css'
 
-const loader = document.querySelector('.loader');
-const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader')
+const gallery = document.querySelector('.gallery')
 
 let simplelightbox = new SimpleLightbox('.gallery-link', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-});
+	captionsData: 'alt',
+	captionPosition: 'bottom',
+	captionDelay: 250,
+})
 
-function createGallery(images) {
-  const markup = images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<li class="gallery-item">
+function buildPhotoGrid(images) {
+	const markup = images
+		.map(
+			({
+				webformatURL,
+				largeImageURL,
+				tags,
+				likes,
+				views,
+				comments,
+				downloads,
+			}) => `<li class="gallery-item">
       <a class="gallery-link" href=${largeImageURL}>
       <div class="image-container">
         <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
@@ -46,23 +46,28 @@ function createGallery(images) {
         </div>
       </a>
     </li>`
-    )
-    .join('');
+		)
+		.join('')
 
-  gallery.innerHTML = markup;
-  simplelightbox.refresh();
+	gallery.innerHTML = markup
+	simplelightbox.refresh()
 }
 
-function clearGallery() {
-  gallery.innerHTML = '';
+function emptyPhotoContainer() {
+	gallery.innerHTML = ''
 }
 
-function showLoader() {
-  loader.classList.remove('hidden');
+function activateSpinner() {
+	loader.classList.remove('hidden')
 }
 
-function hideLoader() {
-  loader.classList.add('hidden');
+function deactivateSpinner() {
+	loader.classList.add('hidden')
 }
 
-export { createGallery, clearGallery, showLoader, hideLoader };
+export {
+	activateSpinner,
+	buildPhotoGrid,
+	deactivateSpinner,
+	emptyPhotoContainer,
+}
